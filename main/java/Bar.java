de8360ac8af01cf48f86ed27ae52182f2ec75bce
Corvigolova2612 <chumin2612@gmail.com>
2022-09-16 15:40:18 +0300

    add new file to the project

--- main/java/Bar.java
+++ main/java/Bar.java
@@ -3,8 +3,12 @@ import java.awt.*;
 public class Bar {
     public static  final int WIDTH = 30; //константа 2-3
     public static final int HEIGHT = 10;
+    public static final int SIDE_WITH = 20;
+
     public static Color bar_main_color = Color.RED;
     public static Color bar_side_color = Color.BLACK;
+
+    public int x; //кордината платформы
 
     public int lives = 3; //биты
 
@@ -32,11 +36,22 @@ public class Bar {
     public void paintBar(Graphics2D graphics){
         graphics.setColor(bar_side_color);
         graphics.fillRoundRect(0,0,0,0,0,0);
+
         graphics.setColor(bar_main_color);
         graphics.fillRoundRect(0,0,0,0,0,0);
+
         graphics.setColor(bar_side_color);
         graphics.fillRoundRect(0,0,0,0,0,0);
     }
+
+    public int getTopY() {return 0;}
+
+    public Rectangle getBoundsRite() {return new Rectangle(0,0,0,0);}
+
+    public Rectangle getBoundsLeft() {return new Rectangle(0,0,0,0);}
+
+    public Rectangle getBounds() {return new Rectangle(0,0,0,0);}
+
 
 }
 /**
--- main/java/bricks.java
+++ main/java/bricks.java
@@ -1,10 +1,33 @@
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class bricks {
-    int[] array = new int[10];
+    //int[] array = new int[10];
+    public ArrayList<ArrayList<Brick>> brickRow = new ArrayList<>();
 
-    public List<Integer> arrayList = new ArrayList<>();
+    public List<Brick> bricks = new ArrayList<>();
+
+    private Arakanoid game;
+
+    public bricks(Arakanoid game) {
+        this.game = game;
+    }
+
+    public static class Brick {
+        private static final int WIDTH = 15;
+        public static final int HEIGHT = 10;
+        int x = 15;
+        int y = 10;
+
+        Color color = Color.BLUE;
+    }
+
+    public void paint(Graphics2D graphics) {
+        for (int i = 0; i < 0; i++)
+            for (int j = 0; j < 0; j++) {
+            }
+    }
 }
 /**
  *Bricks is a class where Brick variants are created for the game Arkanoid
